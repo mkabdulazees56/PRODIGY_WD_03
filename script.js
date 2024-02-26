2f60a4a4f614e41e6af846c61e6257fd9a3dd547
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-02-26 16:22:35 +0500

    project setup

---
+++ script.js
@@ -0,0 +1,41 @@
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleClick(index) {
+    if (!gameActive || gameBoard[index] !== '') return;
+
+    gameBoard[index] = currentPlayer;
+    document.getElementsByClassName('cell')[index].innerText = currentPlayer;
+
+    if (checkWin()) {
+        document.getElementById('result').innerText = `Player ${currentPlayer} wins!`;
+        gameActive = false;
+    } else if (gameBoard.every(cell => cell !== '')) {
+        document.getElementById('result').innerText = 'It\'s a draw!';
+        gameActive = false;
+    } else {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+}
+
+function checkWin() {
+    for (const combo of winningCombinations) {
+        const [a, b, c] = combo;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return true;
+        }
+    }
+    return false;
+}
